0b1b121adee69020ed3b288e51bc6dce8da6bc09
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2024-12-23 00:47:43 -0300

    seed database

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -8,13 +11,40 @@ export async function createAccount(app: FastifyInstance) {
     {
       schema: {
         body: z.object({
+          name: z.string(),
           email: z.string().email(),
           password: z.string().min(6),
         }),
       },
     },
-    () => {
-      return { message: 'User created' }
+    async (request, reply) => {
+      const { email, name, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          error: 'Email already in use',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          email,
+          name,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send({
+        message: 'User created',
+      })
     },
   )
 }
