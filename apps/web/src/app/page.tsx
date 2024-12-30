a9ee00e4ba9bd66f349fa984f5d340c7f0be83eb
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2024-12-30 20:39:17 -0300

    feat: sign-in page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <Button>Button</Button>
+    </div>
+  )
 }
