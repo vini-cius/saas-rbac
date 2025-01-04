4b9f97f79bcfa64d41cea3824e49b4fc7efdfd40
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2025-01-04 19:31:28 -0300

    feat(web): remove member

--- apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
@@ -1,5 +1,5 @@
 import { organizationSchema } from '@saas/auth'
-import { ArrowLeftRightIcon, CrownIcon } from 'lucide-react'
+import { ArrowLeftRightIcon, CrownIcon, UserMinusIcon } from 'lucide-react'
 import Image from 'next/image'
 
 import { ability, getCurrentOrg } from '@/auth/auth'
@@ -9,6 +9,8 @@ import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
 import { getMembers } from '@/http/get-members'
 import { getMembership } from '@/http/get-membership'
 import { getOrganization } from '@/http/get-organization'
+
+import { removeMemberAction } from './actions'
 
 export async function MemberList() {
   const currentOrg = await getCurrentOrg()
@@ -75,6 +77,23 @@ export async function MemberList() {
                         <ArrowLeftRightIcon className="mr-2 size-4" />
                         Transfer Ownership
                       </Button>
+                    )}
+
+                    {permissions?.can('delete', 'User') && (
+                      <form action={removeMemberAction.bind(null, member.id)}>
+                        <Button
+                          type="submit"
+                          size="sm"
+                          variant="destructive"
+                          disabled={
+                            member.userId === membership?.userId ||
+                            member.userId === organization?.ownerId
+                          }
+                        >
+                          <UserMinusIcon className="mr-2 size-4" />
+                          Remove
+                        </Button>
+                      </form>
                     )}
                   </div>
                 </TableCell>
