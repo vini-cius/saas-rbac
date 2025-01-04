a60cf2f90ed285a2a062351b60752e3018a1e929
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2025-01-04 20:26:16 -0300

    feat(web): invites

--- apps/web/src/app/(app)/org/[slug]/members/actions.ts
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -1,11 +1,59 @@
 'use server'
 
-import type { Role } from '@saas/auth'
+import { Role, roleSchema } from '@saas/auth'
+import { HTTPError } from 'ky'
 import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
 
 import { getCurrentOrg } from '@/auth/auth'
+import { createInvite } from '@/http/create-invite'
 import { removeMember } from '@/http/remove-member'
+import { revokeInvite } from '@/http/revoke-invite'
 import { updateMember } from '@/http/update-member'
+
+const inviteSchema = z.object({
+  email: z.string().email({ message: 'Invalid e-mail address.' }),
+  role: roleSchema,
+})
+
+export async function createInviteAction(data: FormData) {
+  const currentOrg = await getCurrentOrg()!
+
+  const result = inviteSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return { success: false, message: null, errors }
+  }
+
+  const { email, role } = result.data
+
+  try {
+    await createInvite({
+      org: currentOrg!,
+      email,
+      role,
+    })
+
+    revalidateTag(`${currentOrg}/invites`)
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+      return { success: false, message, errors: null }
+    }
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+  return {
+    success: true,
+    message: 'Successfully created the invite.',
+    errors: null,
+  }
+}
 
 export async function removeMemberAction(memberId: string) {
   const currentOrg = await getCurrentOrg()
@@ -21,4 +69,15 @@ export async function updateMemberAction(memberId: string, role: Role) {
   await updateMember({ role, org: currentOrg!, memberId })
 
   revalidateTag(`${currentOrg}/members`)
+}
+
+export async function revokeInviteAction(inviteId: string) {
+  const currentOrg = await getCurrentOrg()
+
+  await revokeInvite({
+    org: currentOrg!,
+    inviteId,
+  })
+
+  revalidateTag(`${currentOrg}/invites`)
 }
